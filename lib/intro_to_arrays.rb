5f72fd88dc958da59aa7214d4d01f69e4fca076f
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-13 18:37:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,12 +1,12 @@
 def instantiate_new_array
- my_new_array = []
+ @my_new_array = []
  end
  
  def array_with_two_elements
-   array_two = ["one","two"]
+   @array_two = ["one","two"]
  end
  
  def first_element(array)
-   cats = ["misty","kitty","ash"]
+   @cats = ["misty","kitty","ash"]
    cats[0]
  end
